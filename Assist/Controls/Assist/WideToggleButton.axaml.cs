805f0977455521a2879029014d225812457993e9
Yassine <166349232+yassine808@users.noreply.github.com>
2026-08-29 13:47:00 +0100

    deleted

--- Assist/Controls/Assist/WideToggleButton.axaml.cs
+++
@@ -1,9 +0,0 @@
-﻿using Avalonia;
-using Avalonia.Controls;
-using Avalonia.Controls.Primitives;
-
-namespace Assist.Controls.Assist;
-
-public class WideToggleButton : RadioButton
-{
-}
--- Assist/Controls/Dashboard/ArticleContainerButton.axaml.cs
+++
@@ -1,9 +0,0 @@
-﻿using Avalonia;
-using Avalonia.Controls;
-using Avalonia.Controls.Primitives;
-
-namespace Assist.Controls.Dashboard;
-
-public class ArticleContainerButton : RadioButton
-{
-}
